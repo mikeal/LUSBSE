dd956c66154bcc5ee7ac9101d651dc3d01823b2e
Mikeal Rogers <mikeal.rogers@gmail.com>
2024-07-21 11:10:10 -0700

    better website build

--- build-website.zsh
+++ build-website.zsh
@@ -1,10 +1,34 @@
 #!/usr/bin/env zsh
 
+# Function to extract the first header from a markdown file
+extract_title() {
+    local input_file=$1
+    # Use grep to find the first header (line starting with '#' or '##'), then use sed to remove the leading '# ' or '## '.
+    local title=$(grep -m 1 "^#\{1,2\} " "$input_file" | sed 's/^#\{1,2\} //')
+    echo "$title"
+}
+
 # Function to convert markdown files to HTML
 convert_md_to_html() {
     local input_file=$1
     local output_file=$2
-    pandoc "$input_file" -o "$output_file" --css="../translation.css"
+    local title=$(extract_title "$input_file")
+
+    if [[ "$input_file" == "README.md" ]]; then
+        local css_path="site.css"
+    else
+        local depth=$(echo "$output_file" | awk -F'/' '{print NF-1}')
+        local css_path=$(printf '../%.0s' $(seq 1 $depth))site.css
+    fi
+
+    # Enable markdown extensions and generate table of contents
+    pandoc "$input_file" -o "$output_file" \
+        --css="$css_path" \
+        --toc \
+        --highlight-style=pygments \
+        --metadata title="$title" \
+        --template=./template.html \
+        --from=markdown+footnotes+hard_line_breaks+smart+pipe_tables+fenced_code_blocks
 }
 
 # Create website directory if it does not exist
@@ -17,7 +41,7 @@ for md_file in $(find sutras -name '*.md'); do
     convert_md_to_html "$md_file" "$html_file"
 done
 
-# Convert README.md to index.html
+# Convert README.md to index.html with additional metadata
 convert_md_to_html "README.md" "website/index.html"
 
 echo "Website generation complete!"
